ad6d6a7f8a93c55a24d35bac512f97eb359b2709
aicryptoken <aicryptoken@gmail.com>
2025-05-27 23:45:01 +0800

    update

--- ideapod_fetch.py
+++ ideapod_fetch.py
@@ -108,6 +108,9 @@ def load_and_prepare_data(catering_file, space_file, member_file, product_file, 
         lambda x: '图书馆专注区' if x == '图书馆专注' else x
     )
 
+    # 对于 member_df，如果手机号有重复，只保留"会员号"数值最大的那一条
+    member_df = member_df.sort_values("会员号", ascending=False).drop_duplicates(subset=["手机号"], keep="first")
+
     # 设置主键
     catering_df.set_index("会员号", inplace=True)
     member_df.set_index("会员号", inplace=True)
